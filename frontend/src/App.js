f88b7136e7caa868fe59f455462c7ca6735db323
kingmckevin <kingmckevin@gmail.com>
2022-10-12 06:13:23 +0800

    Added another route for the applicant details component and employee component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,8 @@ import data from "./ContextApi";
 import Home from "./components/Home";
 import { Register } from "./components/Register";
 import EmployeeDetails from "./components/EmployeeDetails";
-import CreateUser from "./components/CreateUser";
+import ApplicantDetails from "./components/ApplicantDetails";
+import Employee from "./components/Employee";
 
 function App() {
   const [userData, setUserData] = useState();
@@ -17,7 +18,25 @@ function App() {
           <Routes>
             <Route
               path="/home"
-              element={userData && userData._id ? <Home /> : <Login />}
+              element={
+                userData && userData._id ? <Home /> || <Register /> : <Login />
+              }
+            />
+            <Route
+              path="/employees"
+              element={
+                userData && userData._id ? <EmployeeDetails /> : <Login />
+              }
+            />
+            <Route
+              path="/employees/:email"
+              element={userData && userData._id ? <Employee /> : <Login />}
+            />
+            <Route
+              path="/applicants"
+              element={
+                userData && userData._id ? <ApplicantDetails /> : <Login />
+              }
             />
             <Route
               path="/register"
